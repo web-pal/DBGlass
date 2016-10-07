cad876b70b7acbff9198cfee46bdaf0f26d18116
Vladimir Pal (BeDoIt) <mail.vpal@gmail.com>
2016-10-07 12:17:13 +0300

    Fix external link opener. Change system app menus.

--- app/components/Connect/Content/ReduxFormBase.js
+++ app/components/Connect/Content/ReduxFormBase.js
@@ -1,27 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReduxFormMain from './ReduxFormMain';
-import check from './UpdatesModal/versionCheck.js';
 
 const { shell } = require('electron');
 
-const ReduxFormBase = () => (
-  <div className="form-wrapper flex-row flex--center">
-    <div className="form">
-      <ReduxFormMain />
-    </div>
-    <span id="madeby">designed by</span>
-    <a
-      onClick={() => shell.openExternal('http://web-pal.com', { activate: false })}
-      className="webpal-logo"
-      href=""
-    >
-      <img
-        src="styles/images/WebPal.svg"
-        role="presentation"
-        style={{ width: '100px' }}
-      />
-    </a>
-  </div>
-);
+
+class ReduxFormBase extends Component {
+  static onClick(ev) {
+    ev.preventDefault();
+    shell.openExternal('http://web-pal.com?ref=dbglass');
+  }
+
+  render() {
+    return (
+      <div className="form-wrapper flex-row flex--center">
+        <div className="form">
+          <ReduxFormMain />
+        </div>
+        <span id="madeby">designed by</span>
+        <a
+          onClick={ReduxFormBase.onClick}
+          className="webpal-logo"
+          href=""
+        >
+          <img
+            src="styles/images/WebPal.svg"
+            role="presentation"
+            style={{ width: '100px' }}
+          />
+        </a>
+      </div>
+    );
+  }
+}
+
 
 export default ReduxFormBase;
