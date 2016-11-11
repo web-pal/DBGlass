f8d85abf092e1f1a749797e04555e5ebb20f6152
Yuichiro Take <y.takey@gmail.com>
2016-11-11 15:24:36 +0900

    Slide new window to right bottom

--- WindowManager.js
+++ WindowManager.js
@@ -1,12 +1,13 @@
 import { BrowserWindow } from 'electron'; // eslint-disable-line import/extensions
 
-const defaultWindowSetting = {
+const defaultWindowOptions = {
   show: false,
   width: 1600,
   height: 900,
   minWidth: 800,
   minHeight: 400
 };
+const windowOffset = 50;
 
 export default class WindowManager {
   constructor() {
@@ -14,7 +15,7 @@ export default class WindowManager {
   }
 
   createWindow() {
-    const w = new BrowserWindow(defaultWindowSetting);
+    const w = new BrowserWindow(this.windowOptions());
     const id = w.id;
 
     w.loadURL(`file://${__dirname}/app/app.html`);
@@ -28,6 +29,19 @@ export default class WindowManager {
     this.windows.set(id, w);
 
     return w;
+  }
+
+  windowOptions() {
+    if (this.isEmpty()) {
+      return defaultWindowOptions;
+    }
+    // copy current window's properties to new window
+    const currentWindow = BrowserWindow.getFocusedWindow();
+    const [width, height] = currentWindow.getSize();
+    const [x, y] = currentWindow.getPosition();
+    const relativeOptions = { width, height, x: x + windowOffset, y: y + windowOffset };
+
+    return Object.assign({}, defaultWindowOptions, relativeOptions);
   }
 
   isEmpty() {
