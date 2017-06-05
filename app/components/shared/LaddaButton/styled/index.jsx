e662df26852e2238ff5b82dec303d9e597292ff5
alexrembo <puskovolexandr12@gmail.com>
2017-06-05 13:24:11 +0300

    fix button bug

--- app/components/shared/LaddaButton/styled/index.jsx
+++ app/components/shared/LaddaButton/styled/index.jsx
@@ -3,9 +3,14 @@ import { Button } from '../../styled';
 
 export const LButton = styled(Button)`
   justify-content: space-around;
+  padding: 0 15px;
 `;
 export const Spinner = styled.img`
   width: 27px;
   margin-left: 5px;
   display: ${props => props.isLooading ? 'block' : 'none'}
+`;
+
+export const Title = styled.span`
+  padding: 8px 0 5px;  
 `;
